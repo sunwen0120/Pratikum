53e27d16c2cb6ff068a8e06e2c5960f5548886f7
Wen <57070886+sunwen0120@users.noreply.github.com>
2020-04-07 02:43:02 +0200

    web crawling

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-import requests #导入requests包
-from bs4 import BeautifulSoup 
-url = 'https://www.chefkoch.de/user/profil/8cfc51bb8fe05e5ce1aac6e823b16dbf/Tibby.html'
-strhtml = requests.get(url)        #Get方式获取网页数据
-print(strhtml.text)
+import requests
+from bs4 import BeautifulSoup
+import urllib
+
+dict = {
+    "categorien": [],
+    "recipe_name": [],
+    "avg_score": [],
+    "difficulty": [],
+    "rating_count": [],
+    "ingredient": [],
+    "user_url": [],
+    "pre_time": []
+}
+
+url = 'https://www.chefkoch.de/rs/s0g26/Beilagen-Rezepte.html'
+r = requests.get(url)
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, "html.parser")
+    # print(soup)
+
+for head in soup.find_all("h2", {"class": "ds-h3 ds-heading-link"}):
+    recipe_name = head.get_text()
+    print(recipe_name)
+
+b = 'https://www.chefkoch.de/rezepte/1728511281966781/Semmelknoedel-aus-der-Form.html'
+r = requests.get(b)
+if r.status_code == 200:
+    soup = BeautifulSoup(r.content, "html.parser")
+
+for head in soup.find_all("div", {"class": "ds-rating-count"}):
+    for
+    rating_count = head.get_text()
+    print(rating_count)
+
+
+
+
---
+++ recipe.py
@@ -0,0 +1,6 @@
+
+url = 'https://www.chefkoch.de/rezepte/2642351415194117/Die-perfekten-Hamburgerbroetchen.html'
+
+
+
+
