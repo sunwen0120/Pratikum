171524bd5bb7325353f1ec4705a072b1d31820b5
Wen <57070886+sunwen0120@users.noreply.github.com>
2020-04-10 00:56:07 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+
 import requests
 from bs4 import BeautifulSoup
 import urllib
+import re
 
 dict = {
     "categorien": [],
@@ -19,20 +21,62 @@ if r.status_code == 200:
     soup = BeautifulSoup(r.content, "html.parser")
     # print(soup)
 
-for head in soup.find_all("h2", {"class": "ds-h3 ds-heading-link"}):
-    recipe_name = head.get_text()
-    print(recipe_name)
+# crawl the recipe name
+# for head in soup.find_all("h2", {"class": "ds-h3 ds-heading-link"}):
+#     recipe_name = head.get_text()
+#     print(recipe_name)
 
-b = 'https://www.chefkoch.de/rezepte/1728511281966781/Semmelknoedel-aus-der-Form.html'
-r = requests.get(b)
-if r.status_code == 200:
-    soup = BeautifulSoup(r.content, "html.parser")
+b = 'https://www.chefkoch.de/rezepte/1778601287739038/Rosmarinkartoffeln-aus-dem-Ofen.html'
+r1 = requests.get(b)
+if r1.status_code == 200:
+    soup = BeautifulSoup(r1.content, "html.parser")
 
-for head in soup.find_all("div", {"class": "ds-rating-count"}):
-    for
-    rating_count = head.get_text()
-    print(rating_count)
+# the name of recipes
+recipe_name = soup.find('h1')
+print(recipe_name.get_text())
 
+# crawel the ingredient of recipe
+table = soup.find('table', {"class": "ingredients table-header"})
+ingredient_table = []
+for tr in table.findAll('tr'):
+    td = tr.findAll('td')
+    ingredient_table.append({
+        "ingredient": td[1].get_text(),
+        "amount": td[0].get_text()
+    })
+print(ingredient_table)
 
+# crawl the average score of recipe
+rating1 = soup.find('div', {"class": "ds-rating-avg"})
+avg_score = rating1.find('strong').get_text()
 
+print(avg_score)
+
+# crawl the number of users, who make evaluation
+rating2 = soup.find('div', {"class": "ds-rating-count"})
+rating_count = rating2.find('strong').get_text()
+print(rating_count)
+
+# crawl the prepare time of recipe
+t = soup.find('span', {"class": "recipe-preptime"}).get_text()
+pre_time = re.findall('[A-Za-z0-9]', t)
+pre_time = ''.join(pre_time)
+print(pre_time)
+
+# time = t.next_sibling
+# prog = re.compile('[A-Za-z0-9]')
+# time = prog.findall(t)
+# print(t.next_sibling.strip())
+
+# difficulty of recipe
+diff = soup.find('span', {"class": "recipe-difficulty"}).get_text()
+difficulty = re.findall('[A-Za-z0-9]', diff)
+difficulty = ''.join(difficulty)
+print(difficulty)
+
+# crawl the calorie of the recipe
+caro = soup.find('span', {"class": "recipe-kcalories"}).get_text()
+calorie = re.findall('[A-Za-z0-9]', caro)
+calorie = ''.join(calorie)
+print(calorie)
 
--- recipe.py
+++ recipe.py
@@ -1,6 +1,38 @@
 
-url = 'https://www.chefkoch.de/rezepte/2642351415194117/Die-perfekten-Hamburgerbroetchen.html'
+import requests
+from bs4 import BeautifulSoup
+import urllib
 
+url = "https://www.chefkoch.de/rezepte/kategorien/"
+headers = {'user-agent':'Mozilla/5.0'}
 
+def categorien_name(url):
+    cat_name=[]
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.content, "html.parser")
 
+    for categorien in soup.find_all('h2', {'class', 'category-level-1'}):
+        # cat_name = categorien.get_text()
+        cat_name.append({
+            "categorien name":categorien.get_text().lstrip().rstrip()
+        })
+    return(cat_name)
 
+print(categorien_name(url))
+
+# link = soup.find('div',{'class', 'category-column'}).find('h2').find('a')['href']
+# # print(link)
+#
+# r = requests.get("https://www.chefkoch.de"+link, headers=headers)
+# if r.status_code == 200:
+#     link_soup = BeautifulSoup(r.content, "html.parser")
+#     # print(link_soup)
+# koch_link = link_soup.find('a',{'class', 'ds-mb ds-mb-row ds-card rsel-recipe bi-recipe-item'}).get('href')
+#
+# r = requests.get(koch_link, headers=headers)
+# if r.status_code == 200:
+#     detail_soup = BeautifulSoup(r.content, "html.parser")
+#     print(detail_soup)
+#
+# def get_recipe_info():
